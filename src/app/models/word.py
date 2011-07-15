53a0d4249f9bf0b6774e609e1b7608c39c6542f9
OHTA Shogo <athos0220@gmail.com>
2011-07-15 12:14:22 +0900

    added WordList and WordMember models

--- src/app/models/word.py
+++ src/app/models/word.py
@@ -3,3 +3,11 @@ from google.appengine.ext import db
 class Word(db.Model):
     spelling = db.StringProperty()
     meaning = db.StringProperty()
+
+class WordList(db.Model):
+    name = db.StringProperty()
+    owner = db.StringProperty() # FIXME: it should be db.UserProperty()
+
+class WordMember(db.Model):
+    wordlist = db.ReferenceProperty(WordList, collection_name='words')
+    word = db.ReferenceProperty(Word)
