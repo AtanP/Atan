a768739ad850a22023b30de807adf31e00320d3f
OHTA Shogo <athos0220@gmail.com>
2011-07-15 12:25:11 +0900

    introduced WordList into the related view and controller

--- src/app/handlers/words.py
+++ src/app/handlers/words.py
@@ -2,14 +2,17 @@ from os.path import dirname, join
 
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
-from app.models.word import Word
+from app.models.word import Word, WordList, WordMember
 
 class Words(webapp.RequestHandler):
     def get(self):
-        words_query = Word.all()
-        words = words_query.fetch(1000)
+        wordlist_key = self.request.get('wordlist_key')
+        if wordlist_key:
+            wordlist = WordList.get(wordlist_key)
+        else:
+            wordlist = WordList.all().get()
 
-        template_values = {'words': words}
+        template_values = {'words': wordlist.words, 'wordlists': WordList.all(), 'wordlist_name': wordlist.name}
 
         path = join(dirname(dirname(dirname(__file__))), 'template', 'words.html')
         self.response.out.write(template.render(path,template_values))
